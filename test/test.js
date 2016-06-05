21b05ad3261600df615a78f393777a53ed14554d
nmaaruf <nabaz.maaruf@aenetworks.com>
2016-06-05 19:56:12 -0400

    changed test again

--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
 var request = require('supertest');
 var app = require('../app.js');
+
 describe('GET /', function() {
-  it('hello github test', function(done) {
-    request(app).get('/').expect('hello github test', done);
+  it('respond with hello world', function(done) {
+    request(app).get('/').expect('hello jenkins', done);
   });
 });
