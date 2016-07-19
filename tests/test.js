c48e6dfd43db59c4dc70b61d5e62b124847b30f8
Maaruf <nmarruf@ny-m104-529338.aetvn.com>
2016-07-19 12:21:20 -0400

    laravel

--- tests/test.js
+++ tests/test.js
@@ -2,6 +2,6 @@ var request = require('supertest');
 var app = require('../app.js');
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello jenkins', done);
+    request(app).get('/').expect('hello jenkins1', done);
   });
 });
